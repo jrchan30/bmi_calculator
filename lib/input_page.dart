0294f60245d2eb2b2278ad31ee3ce249ef4a67d0
Russell Chan <jrussellchan2000@gmail.com>
2021-07-23 13:16:21 +0700

    refactor icon content and reusable card into seperate files

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,8 @@
+import 'package:bmi_calculator/reusable_card.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import 'icon_content.dart';
 
 const bottomContainerHeight = 80.0;
 const bottomContainerColor = Color(0xFFEB1555);
@@ -70,50 +73,6 @@ class _InputPageState extends State<InputPage> {
           margin: EdgeInsets.only(top: 10),
         )
       ]),
-    );
-  }
-}
-
-class IconContent extends StatelessWidget {
-  IconContent({required this.icon, required this.label});
-
-  final IconData icon;
-  final String label;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(
-          icon,
-          size: 80,
-        ),
-        SizedBox(height: 15.0),
-        Text(
-          label,
-          style: TextStyle(fontSize: 18, color: Color(0xFF8D8E98)),
-        )
-      ],
-    );
-  }
-}
-
-class ReusableCard extends StatelessWidget {
-  ReusableCard({required this.color, this.cardChild});
-
-  final Color color;
-  final Widget? cardChild;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: cardChild,
-      margin: EdgeInsets.all(15),
-      decoration: BoxDecoration(
-        color: color,
-        borderRadius: BorderRadius.circular(10),
-      ),
     );
   }
 }
