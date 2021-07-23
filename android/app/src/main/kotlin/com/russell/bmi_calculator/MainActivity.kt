42e6ca2b6448b95b57c89aa8da47cb9437d030da
unknown <jrussellchan2000@gmail.com>
2021-07-23 13:11:15 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/russell/bmi_calculator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.russell.bmi_calculator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/input_page.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+const bottomContainerHeight = 80.0;
+const bottomContainerColor = Color(0xFFEB1555);
+const activeCardColor = Color(0xFFF1D1E33);
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text('BMI CALCULATOR')),
+      ),
+      body: Column(children: [
+        Expanded(
+          child: Row(
+            children: [
+              Expanded(
+                child: ReusableCard(
+                  color: activeCardColor,
+                  cardChild: IconContent(
+                    icon: FontAwesomeIcons.mars,
+                    label: 'MALE',
+                  ),
+                ),
+              ),
+              Expanded(
+                child: ReusableCard(
+                  color: activeCardColor,
+                  cardChild: IconContent(
+                    icon: FontAwesomeIcons.venus,
+                    label: 'FEMALE',
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          child: ReusableCard(
+            color: activeCardColor,
+          ),
+        ),
+        Expanded(
+          child: Row(
+            children: [
+              Expanded(
+                child: ReusableCard(
+                  color: activeCardColor,
+                ),
+              ),
+              Expanded(
+                child: ReusableCard(
+                  color: activeCardColor,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Container(
+          color: bottomContainerColor,
+          width: double.infinity,
+          height: bottomContainerHeight,
+          margin: EdgeInsets.only(top: 10),
+        )
+      ]),
+    );
+  }
+}
+
+class IconContent extends StatelessWidget {
+  IconContent({required this.icon, required this.label});
+
+  final IconData icon;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          icon,
+          size: 80,
+        ),
+        SizedBox(height: 15.0),
+        Text(
+          label,
+          style: TextStyle(fontSize: 18, color: Color(0xFF8D8E98)),
+        )
+      ],
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  ReusableCard({required this.color, this.cardChild});
+
+  final Color color;
+  final Widget? cardChild;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: cardChild,
+      margin: EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(10),
+      ),
+    );
+  }
+}
